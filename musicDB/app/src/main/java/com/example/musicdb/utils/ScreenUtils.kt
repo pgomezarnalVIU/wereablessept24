77798d2be10adc85f9d6bdc55d3c46bb56c3f36c
Paco Mastermind <paco@mastermind.ac>
2024-10-14 15:47:51 +0200

    Clase jet pack compose

--- musicDB/app/src/main/java/com/example/musicdb/utils/ScreenUtils.kt
+++
@@ -1,6 +0,0 @@
-package com.knowledgespike.books.utils
-
-sealed class Screen (val route: String){
-    data object BooksListScreen : Screen("books_list_screen")
-    data object AddEditBooksScreen : Screen("add_edit_book_screen")
-}
