b17ff69d3fa05c185fd22db96a80745e1c8379b5
Paco Mastermind <paco@mastermind.ac>
2024-11-03 19:40:37 +0100

    Settings controller

--- flutter/mis_imagenes/lib/screens/settings_screen.dart
+++ flutter/mis_imagenes/lib/screens/settings_screen.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:mis_imagenes/data/sp_helper.dart";
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -8,11 +9,36 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<SettingsScreen> {
+  final TextEditingController txtName = TextEditingController();
+  final List<String> _images = ['Lake', 'Mountain', 'Sea', 'Country'];
+  String _selectedImage = 'Lake';
+  final SPHelper helper = SPHelper();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Settings")),
-      body: const Column(children: []),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: txtName,
+              decoration: const InputDecoration(hintText: 'Enter your name'),
+            ),
+            DropdownButton<String>(
+                value: _selectedImage,
+                items: _images.map((String value) {
+                  return DropdownMenuItem(value: value, child: Text(value));
+                }).toList(),
+                onChanged: (newvalue) {
+                  setState(() {
+                    _selectedImage = newvalue ?? 'Lake';
+                  });
+                }),
+          ],
+        ),
+      ),
     );
   }
 }
