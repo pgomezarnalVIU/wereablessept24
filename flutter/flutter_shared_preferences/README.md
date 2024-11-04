81d97e0cddca6f15a8bc5bf83ab061745edaf731
Paco Mastermind <paco@mastermind.ac>
2024-11-04 09:02:23 +0100

    Http GET

--- flutter/flutter_shared_preferences/README.md
+++
@@ -1,3 +0,0 @@
-# mindful_app
-
-A new Flutter project.
--- flutter/flutter_shared_preferences/android/app/src/main/kotlin/com/example/flutter_shared_preferences/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_shared_preferences
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
